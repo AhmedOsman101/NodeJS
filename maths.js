40280959c1f9fd35284eb4b76fb429f0228125c8
AhmedOsman101 <trollingosman101@outlook.com>
2023-10-24 21:31:04 +0300

    made some changes

---
+++ maths.js
@@ -0,0 +1,16 @@
+const methods = {
+	plus: function (num1, num2) {
+		return num1 + num2;
+	},
+	minus: function (num, minus) {
+		return num - minus;
+	},
+	multiply: function (num, multi) {
+		return num * multi;
+	},
+	divide: function (num, divider) {
+		return num / divider;
+	},
+};
+
+module.exports = methods;
---
+++ test.js
@@ -0,0 +1,6 @@
+const maths = require("./maths.js");
+
+maths.plus(2, 5);
+maths.minus(2, 5);
+maths.multiply(2, 5);
+maths.divide(2, 5);
