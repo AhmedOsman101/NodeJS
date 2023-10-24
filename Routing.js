2fa31f04aa194db4c50cf292c9d2f009d17ed3c9
AhmedOsman101 <trollingosman101@outlook.com>
2023-10-24 20:51:19 +0300

    formatting

--- Routing.js
+++ Routing.js
@@ -5,17 +5,17 @@ const http = require("http");
 const home = fs.readFileSync("./index.html");
 
 const server = http.createServer((req, res) => {
-    if (req.method === "GET") {
-        if (req.url === "/") {
-            res.end(home);
-        } else if (req.url === "/about") {
-            res.end('<p style="font-size:5rem">About</p>');
-        } else {
-            res.end("ERROR_404 NOT FOUND");
-        }
-    }
+	if (req.method === "GET") {
+		if (req.url === "/") {
+			res.end(home);
+		} else if (req.url === "/about") {
+			res.end('<p style="font-size:5rem">About</p>');
+		} else {
+			res.end("ERROR_404 NOT FOUND");
+		}
+	}
 });
 
 server.listen(3000, () => {
-    console.log("http://localhost:3000/");
+	console.log("http://localhost:3000/");
 });
