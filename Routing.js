081a74bcc3c85bfa01ca88d91d8db1e9144adbd1
AhmedOsman101 <trollingosman101@outlook.com>
2023-10-25 10:44:51 +0300

    creating API with JSON

--- Routing.js
+++ Routing.js
@@ -1,21 +1,42 @@
-const fs = require("fs");
-const os = require("os");
 const http = require("http");
 
-const home = fs.readFileSync("./index.html");
+const API = [
+    {
+        id: 1,
+        name: "Othman",
+        age: 18,
+        gender: "male",
+    },
+    {
+        id: 2,
+        name: "Adam",
+        age: 18,
+        gender: "male",
+    },
+    {
+        id: 3,
+        name: "Amir Sal3at",
+        age: 16,
+        gender: "female",
+    },
+    {
+        id: 4,
+        name: "3wees",
+        age: 17,
+        gender: "male",
+    },
+];
 
 const server = http.createServer((req, res) => {
-	if (req.method === "GET") {
-		if (req.url === "/") {
-			res.end(home);
-		} else if (req.url === "/about") {
-			res.end('<p style="font-size:5rem">About</p>');
-		} else {
-			res.end("ERROR_404 NOT FOUND");
-		}
-	}
+    if (req.method === "GET") {
+        if (req.url === "/") {
+            res.end(`${JSON.stringify(API)}`);
+        } else {
+            res.end("<h1 font='80'>ERROR404 - PAGE NOT FOUND</h1>");
+        }
+    }
 });
 
 server.listen(3000, () => {
-	console.log("http://localhost:3000/");
+    console.log("server is running on port: http://localhost:3000/");
 });
