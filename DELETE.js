b28da51c0f098f88545ec15744741d4bea7646c2
AhmedOsman101 <trollingosman101@outlook.com>
2023-10-31 23:53:52 +0200

    updated DELETE request

--- DELETE.js
+++ DELETE.js
@@ -1,23 +1,39 @@
+// Import the required modules
 const http = require("http");
 const GenUsers = require("./GenerateUsers");
-const port = 5002;
 
+// Define the port on which the server will listen
+const port = 5001;
+
+// create an empty array to hold the users
 let Users = [];
+
+// Generate mock data for 10 users using the GenUsers module
 GenUsers.generateData(Users, 10);
 
+// Create a new HTTP server
 http.createServer((req, res) => {
+	// If a DELETE request is made to "/Delete/User/{id}", delete the user with the given id
 	if (req.url.startsWith("/Delete/User/") && req.method === "DELETE") {
+		// Extract the user id from the URL
 		let userId = +req.url.split("/").at(-1);
+
+		// Find the index of the user with the given id
 		let userIndex = Users.findIndex((user) => {
 			return userId === user.id;
 		});
+
+		// If the user was not found, send an error message
 		if (userIndex === -1) {
 			res.end("User Is Not Found");
-		} else {
+		}
+		// Otherwise, delete the user and send back the updated Users array as a JSON string
+		else {
 			Users.splice(userIndex, 1);
 			res.end(JSON.stringify(Users));
 		}
 	}
+	// Start the server and have it listen on the specified port
 }).listen(port, () => {
 	console.log(`server is running on http://localhost:${port}`);
 });
--- DELETEexpress.js
+++ DELETEexpress.js
@@ -1,35 +1,34 @@
 const express = require("express");
 const fs = require("fs");
 const GenUsers = require("./GenerateUsers");
-const form = fs.readFileSync("./index.html", "utf8");
 
+const form = fs.readFileSync("./index.html", "utf8");
+const port = 5002;
+const App = express();
 let Users = [];
+
 GenUsers.generateData(Users, 10);
 
-const App = express();
 App.use(express.urlencoded({ extended: true }));
 
 App.delete("/User/:id", (req, res) => {
-    let userId = +req.url.split("/").at(-1);
-    let userIndex = Users.findIndex((user) => {
-        return userId === user.id;
-    });
+	let userId = +req.url.split("/").at(-1);
+	let userIndex = Users.findIndex((user) => {
+		return userId === user.id;
+	});
 
-    if (userIndex !== -1) {
-        Users.splice(userIndex, 1)
-        res.send(Users)
-    } else {
-        res.send("User Is Not Found !!!")
-    }
+	if (userIndex !== -1) {
+		Users.splice(userIndex, 1);
+		res.send(Users);
+	} else {
+		res.send("User Is Not Found !!!");
+	}
 });
-
 
 App.use((req, res) => {
-    res.end("User Not Found");
+	res.end("User Not Found");
 });
 
-const port = 1297;
-
 App.listen(port, () => {
-    console.log("done");
+	console.log("done");
 });
