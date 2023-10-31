38ce753bc5318dfca9c573f07695401840a90074
AhmedOsman101 <trollingosman101@outlook.com>
2023-10-31 14:36:23 +0200

    PUT request with express

---
+++ PUTexpress.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const fs = require("fs");
+const form = fs.readFileSync("./index.html", "utf8");
+let Users = [
+    {
+        id: 1,
+        name: "John",
+        age: 25,
+    },
+    {
+        id: 2,
+        name: "Sarah",
+        age: 30,
+    },
+    {
+        id: 3,
+        name: "Michael",
+        age: 35,
+    },
+    {
+        id: 4,
+        name: "Emily",
+        age: 28,
+    },
+    {
+        id: 5,
+        name: "David",
+        age: 32,
+    },
+    {
+        id: 6,
+        name: "Jessica",
+        age: 27,
+    },
+    {
+        id: 7,
+        name: "Daniel",
+        age: 31,
+    },
+    {
+        id: 8,
+        name: "Olivia",
+        age: 29,
+    },
+    {
+        id: 9,
+        name: "Matthew",
+        age: 34,
+    },
+    {
+        id: 10,
+        name: "Sophia",
+        age: 26,
+    },
+];
+
+const App = express();
+App.use(express.json());
+
+App.put("/Update/User/:id", (req, res) => {
+    let userId = +req.url.split("/").at(-1);
+    let userIndex = Users.findIndex((user) => {
+        return userId === user.id;
+    });
+
+    if (userIndex !== -1) {
+        Users[userIndex].name = req.body.name
+            ? req.body.name
+            : Users[userIndex].name;
+        Users[userIndex].age = req.body.name
+            ? req.body.age
+            : Users[userIndex].age;
+        res.json(Users);
+    } else {
+        res.send("User Is Not Found !!!");
+    }
+});
+
+App.use((req, res) => {
+    res.end("User Not Found");
+});
+
+const port = 1295;
+
+App.listen(port, () => {
+    console.log("done");
+});
