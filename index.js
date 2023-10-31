db7519c7ff5709bd185c12f493514f1572ecbe35
AhmedOsman101 <trollingosman101@outlook.com>
2023-10-31 08:16:00 +0200

    ezzat's code

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+const formContent = fs.readFileSync("./form.html");
+
+let Users = [
+    { id: 1, name: "ahmed", age: 16 },
+    { id: 2, name: "mohamed", age: 40 },
+    { id: 3, name: "mustafa", age: 30 },
+];
+
+http.createServer((req, res) => {
+    var data = "";
+    if (req.url === "/form" && req.method === "GET") {
+        res.end(formContent);
+    } else if (req.url === "/adduser" && req.method === "POST") {
+        req.on("data", (chunck) => {
+            data += chunck;
+        });
+        req.on("end", () => {
+            let newData = data.split("&");
+            let obj = {};
+            for (let i = 0; i < newData.length; i++) {
+                let valueKey = newData[i].split("=");
+                obj[valueKey[0]] = valueKey[1];
+            }
+            res.end(JSON.stringify(obj));
+        });
+    }
+}).listen(5000, () => {
+    console.log("running");
+});
