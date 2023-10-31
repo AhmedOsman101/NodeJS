4a497e2114460c639c5bdb7d851211a9a7ede3c6
AhmedOsman101 <trollingosman101@outlook.com>
2023-11-01 00:50:15 +0200

    finished PUT request with express

--- PUTexpress.js
+++ PUTexpress.js
@@ -9,23 +9,37 @@ GenUsers.generateData(Users, 10);
 const App = express();
 App.use(express.json());
 
+// Handle PUT requests to "/Update/User/:id" URL
 App.put("/Update/User/:id", (req, res) => {
-	let userId = +req.url.split("/").at(-1);
+	let data = req.body; // Extract the request body (new user data)
+	let userId = +req.params.id; // Extract the user id from the request parameters
+
+	// Find the index of the user with the given id in the Users array
 	let userIndex = Users.findIndex((user) => {
 		return userId === user.id;
 	});
 
+	// If the user was found in the Users array:
 	if (userIndex !== -1) {
-		Users[userIndex].name = req.body.name || Users[userIndex].name;
-		Users[userIndex].age = req.body.name || Users[userIndex].age;
+		// Update the user's data with the new data, if provided
+		Users[userIndex].username = data.username || Users[userIndex].username;
+		Users[userIndex].email = data.email || Users[userIndex].email;
+		Users[userIndex].password = data.password || Users[userIndex].password;
+		Users[userIndex].phone_number =
+			data.phone_number || Users[userIndex].phone_number;
+
+		// Send back the updated Users array as a response
 		res.json(Users);
-	} else {
-		res.send("User Is Not Found !!!");
+	}
+	// If the user was not found in the Users array:
+	else {
+		res.send("User Doesn't Exist !!!"); // Send back an error message as a response
 	}
 });
 
+// handle any other requests
 App.use((req, res) => {
-	res.end("User Not Found");
+	res.end("ERROR-404 Page Was Not Found"); // Send back an error message as a response
 });
 
 App.listen(port, () => {
