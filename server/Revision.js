76dc1724bffa3f5edb91464eaf7130b39e040803
AhmedOsman101 <trollingosman101@outlook.com>
2023-11-07 11:54:33 +0200

    revision folders

---
+++ server/Revision.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const express = require("express");
+const mysql2 = require("mysql2");
+const fs = require("fs");
+
+const form = fs.readFileSync("../client/Form.html", "utf8");
+const port = 2000;
+
+const connection = mysql2.createConnection({
+    user: "root",
+    password: "",
+    database: "revision",
+    host: "localhost",
+});
+
+http.createServer((req, res) => {
+    if (req.method === "GET") {
+        if (req.url === "/") {
+            connection.execute("SELECT * FROM `products`", (err, data) => {
+                if (err) {
+                    res.writeHead(404, { "Content-Type": "application/json" });
+                    res.end(`ERROR: ${err}`)
+                } else {
+                    res.writeHead(200, { "Content-Type": "application/json" });
+                    res.end(JSON.stringify(data))
+                }
+            });
+        } else if (req.url=== "/form"){
+            res.end(form)
+        }
+    } else {
+    }
+}).listen(port, () => {
+    console.log(`Server Is Running On http://localhost:${port}`);
+});
