1d50d866350b9d8dca7052888a3fd9dee18911ec
AhmadOthman <trollingosman101@outlook.com>
2024-01-06 09:59:11 +0200

    Update package versions, remove unnecessary files, update port number. (generated by blackbox.ai )

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,12 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-target-blank */
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./assets/styles/App.css";
 import Form from "./Components/form";
 
 export default function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
-    <Form></Form>
+			<Form/>
 		</>
 	);
 }
